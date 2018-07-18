76b39f9e77933c5dc860ab61c30522edd72423f1
Ryan <36577405+OregonianPyro@users.noreply.github.com>
2018-07-18 16:59:22 -0700

    Delete ready.js

--- events/ready.js
+++
@@ -1,6 +0,0 @@
-module.exports = (client) => {
-  console.log(client.chalk.bgBlack.greenBright(`${client.user.tag} logged in.`));
-  client.user.setActivity('$help | @Utilidex prefix', {
-    type: 'PLAYING'
-  });
-};
