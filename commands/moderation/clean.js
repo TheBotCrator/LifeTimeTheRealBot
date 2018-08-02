f4c1431dacd0546b51be1748518974072f3d7867
Ryan <36577405+OregonianPyro@users.noreply.github.com>
2018-08-02 14:15:18 -0700

    more classes

--- commands/moderation/clean.js
+++ commands/moderation/clean.js
@@ -1,41 +1,45 @@
+const Command = require('../base/command.js');
 const { RichEmbed } = require("discord.js");
 
-module.exports.run = async (client, message, args) => {
-    const { prefix } = client.settings.get(message.guild.id);
-    if (!message.member.permissions.has('MANAGE_MESSAGES')) return client.permsError.member(client, message, 'MANAGE_MESSAGES');
-    if (!message.guild.me.permissions.has('MANAGE_MESSAGES')) return client.permsError.self(client, message, 'MANAGE_MESSAGES');
-    if (!args[0]) return client.help(message, prefix, 'clean');
-    const amount = args[0];
-    if (amount < 1 || amount > 100 || isNaN(parseInt(amount))) {
-        message.delete();
-        return message.channel.send(`${client.emotes.x} Incorrect input provided.`);
+class Clean extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'clean',
+            category: 'moderation',
+            description: 'Prunes the last `x` amount of message sent by the bot in the current channel.',
+            usage: '{prefix}clean <2-100>',
+            parameters: 'integerNumber',
+            extended: false,
+            enabled: true,
+            reason: null,
+            guildOnly: true,
+            devOnly: false,
+            permission: 'MANAGE_MESSAGES',
+            cooldown: 5,
+            aliases: []
+        });
     };
-    await message.delete();
-    message.channel.fetchMessages({
-        limit: 100,
-    }).then(async (messages) => {
-        const msgs = messages.filter(m => m.author.id === client.user.id).array().slice(0, amount)
-        try {
-            await message.channel.bulkDelete(msgs);
-        }catch(e){
-            return message.channel.send(`${client.emotes.x} Error pruning: \`${e.message}\``);
-        };
-    });
-    return message.channel.send(`${client.emotes.check} Successfully deleted **${amount}** messages from the bot.`).then(m => m.delete(5000));
+
+    async run(message, args) {
+        const { prefix } = this.client.settings.get(message.guild.id);
+        if (!message.member.permissions.has('MANAGE_MESSAGES')) return this.client.permsError.member(this.client, message, 'MANAGE_MESSAGES');
+        if (!message.guild.me.permissions.has('MANAGE_MESSAGES')) return this.client.permsError.self(this.client, message, 'MANAGE_MESSAGES');
+        if (!args[0]) return this.client.usage(message, prefix, 'clean');
+        const amount = args[0];
+        if (amount < 2 || amount > 100 || isNaN(parseInt(amount))) return this.client.argsError(this.client, message, 'Invalid number inputted.');
+        await message.delete();
+        message.channel.fetchMessages({
+            limit: 100,
+        }).then(async (messages) => {
+            const msgs = messages.filter(m => m.author.id === this.client.user.id).array().slice(0, amount)
+            try {
+                await message.channel.bulkDelete(msgs);
+            } catch (e) {
+                return message.channel.send(`${this.client.emotes.x} Error pruning: \`${e.message}\``);
+            };
+        });
+        return message.channel.send(`${this.client.emotes.check} Successfully deleted **${amount}** messages from the bot.`).then(m => m.delete(5000));
+    };
 };
 
-module.exports.conf = {
-    enabled: true,
-    reason: null,
-    permission: 'MANAGE_MESSAGES',
-    devOnly: false
-};
-
-module.exports.help = {
-    name: "clean",
-    category: "MODERATION",
-    description: "Deletes the last `x` amount of messages sent by the bot in the current channel.",
-    usage: "[prefix]clean <number>",
-    parameters: "integerNumber",
-    extended: false
-};
+module.exports = Clean;
